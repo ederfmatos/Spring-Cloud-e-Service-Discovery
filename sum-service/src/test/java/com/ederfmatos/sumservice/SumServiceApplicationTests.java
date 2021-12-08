0dbc1565fd3d88597757a90973a0bc224a6be4f4
Eder Matos <ederfmatos@gmail.com>
2021-12-08 19:45:06 -0300

    Create basic service named Sum-Service

---
+++ sum-service/src/test/java/com/ederfmatos/sumservice/SumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ederfmatos.sumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
